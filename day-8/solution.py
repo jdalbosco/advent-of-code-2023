c88fdab5a07c68ddfbf92933ee56e9da05ecb42f
jdalbosco <juliadalbosco1@gmail.com>
2023-12-09 17:32:26 -0300

    Adding EOF

--- day-8/solution.py
+++ day-8/solution.py
@@ -15,4 +15,3 @@ if __name__ == "__main__":
     directions, network = InputParser.parse(open("inputs/day-8.txt", "r"))
     day_8_part_1(network, directions)
     day_8_part_2(network, directions)
-    
