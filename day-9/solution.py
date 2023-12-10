693e95eb895e29970f32dfd1a8fce8dd9a9357fd
jdalbosco <juliadalbosco1@gmail.com>
2023-12-09 21:05:41 -0300

    Day 9 solution, parts 1-2

--- day-9/solution.py
+++ day-9/solution.py
@@ -1,24 +1,35 @@
+FORWARDS = 1
+BACKWARDS = -1
+
+def parse(line):
+    return [int(value) for value in line.split(' ')]
+
 def calculate_differences(history):
     differences = []
     for i in range(1, len(history)):
         differences.append(history[i] - history[i-1])
     return differences
 
-def calculate_prediction(history):
+def extrapolate(history):
     if all(value == 0 for value in history):
         return 0
-    return history[-1] + calculate_prediction(calculate_differences(history))
+    return history[-1] + extrapolate(calculate_differences(history))
 
-def parse(line):
-    return [int(value) for value in line.split(' ')]
-
-if __name__ == "__main__":
-    input = open("inputs/day-9.txt", "r")
-    
+def sum_predictions(input, direction):
     sum_of_predictions = 0
 
     for line in input:
         history = parse(line)
-        sum_of_predictions += calculate_prediction(history)
+        sum_of_predictions += extrapolate(history[::direction])
 
-    print(sum_of_predictions)
+    return sum_of_predictions
+
+def day_9_part_1(input):
+    print(sum_predictions(input, FORWARDS))
+
+def day_9_part_2(input):
+    print(sum_predictions(input, BACKWARDS))
+
+if __name__ == "__main__":
+    day_9_part_1(open("inputs/day-9.txt", "r"))
+    day_9_part_2(open("inputs/day-9.txt", "r"))
